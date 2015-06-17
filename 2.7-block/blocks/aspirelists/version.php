efd866a641e5ffb76ab8d54cc035e9a6ec0b0c53
tombola <thomas.readings@gmail.com>
2015-06-17 11:32:26 +0100

    duplicate 2.4 block as 2.7

---
+++ 2.7-block/blocks/aspirelists/version.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright (c) Talis Education Limited, 2013
+// Released under the LGPL Licence - http://www.gnu.org/licenses/lgpl.html. Anyone is free to change or redistribute this code.
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 201504020940;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2012120300; // YYYYMMDDHH (This is the release version for Moodle 2.4)
+$plugin->component = 'block_aspirelists';
